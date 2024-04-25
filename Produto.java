96d93c1087f1c9a5c7c3413468af21695e9577d7
Michel <michel.fvc@proton.me>
2024-04-24 21:47:13 -0300

    Add Produto class with getters, setters, and constructor

---
+++ Produto.java
@@ -0,0 +1,38 @@
+public class Produto {
+    private String nome;
+    private double preco;
+    private int id;
+
+    // Construtor
+    public Produto(int id, String nome, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    // Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    // Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
